8437f203e56c92aa1dbbca636d90b2d474716899
Pedro Giuliano Farina <pedrogfarina@gmail.com>
2020-04-07 17:06:05 -0300

    fix

--- Sources/MyFirstPackage/MyFirstPackage.swift
+++ Sources/MyFirstPackage/MyFirstPackage.swift
@@ -1,3 +1,16 @@
-struct MyFirstPackage {
-    var text = "Hello, World!"
+import reflectium_ipsum
+
+public class MyOwnReflectionGenerator {
+    public static func generatiumBiggumReflectium() -> String {
+        return ReflectionGenerator.generatiumReflectium() + " E digo mais! " + ReflectionGenerator.generatiumReflectium()
+    }
+    public static func generatiumWeirdiumReflectium() -> String {
+        var words = ReflectionGenerator.generatiumReflectium().split(separator: " ").shuffled()
+        var phrase: String = String(words.removeFirst())
+        while !words.isEmpty {
+            phrase += " \(words.removeFirst())"
+        }
+        return phrase
+    }
 }
+
