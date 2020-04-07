7e768c4ad784e7f4fcb4c063d18baa67cff10f04
Pedro Giuliano Farina <pedrogfarina@gmail.com>
2020-04-07 17:05:18 -0300

    fix

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "MyFirstPackage",
-            dependencies: []),
+            dependencies: ["reflectium-ipsum"]),
         .testTarget(
             name: "MyFirstPackageTests",
             dependencies: ["MyFirstPackage"]),
--- Tests/MyFirstPackageTests/MyFirstPackageTests.swift
+++ Tests/MyFirstPackageTests/MyFirstPackageTests.swift
@@ -6,7 +6,7 @@ final class MyFirstPackageTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(MyFirstPackage().text, "Hello, World!")
+        
     }
 
     static var allTests = [
